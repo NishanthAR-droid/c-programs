bf43ec9af967fea5b35434c7036f88f48425e189
NishanthAR-droid <85539246+NishanthAR-droid@users.noreply.github.com>
2021-06-13 11:33:21 +0530

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,28 @@
+//program to find the lcm of two numbers using recursion
+#include<stdio.h>
+int lcm(int,int);
+
+int main()
+{
+	int a ,b,result ;
+	printf("Enter any two numbers\n");
+	scanf("%d%d",&a,&b);
+	result=lcm(a,b);
+	printf("The lcm of %d and %d is %d\n",a,b,result);
+	return 0;
+}
+
+int lcm(int a,int b)
+{
+	static int i=1;
+	if(i%a==0 && i%b==0)
+	{
+		return i;
+	}
+	else
+	{
+		i++;
+		lcm(a,b);
+		return i;
+	}
+}
---
+++ prime_num.c
@@ -0,0 +1,41 @@
+//program to find the number entered by the user is prime or not using recursion
+
+#include<stdio.h>
+int isprime(int,int);
+
+int main()
+{
+	int num,prime;
+	printf("Enter a positive number\n");
+	scanf("%d",&num);
+	prime=isprime(num,num/2);
+	if (prime==1)
+	{
+		printf("%d is a prime number\n",num);
+	}
+	else
+	{
+		printf("%d is not a prime number\n",num);
+	}
+}
+
+
+int isprime(int num, int i)
+{
+	if (i==1)
+	{
+		return 1;
+	}
+	else 
+	{
+		if(num%i==0)
+		{
+			return 0;
+		}
+		else
+		{
+			isprime(num,i-1);
+		}
+	}
+	
+}
